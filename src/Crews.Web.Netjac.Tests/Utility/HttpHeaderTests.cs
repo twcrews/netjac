b78b325a2a516ffac419a6ad9909148782bd7a01
Tommy Crews <twcrews96@gmail.com>
2024-09-11 09:40:30 -0500

    Completed `HttpHeader` struct and tests.

--- src/Crews.Web.Netjac.Tests/Utility/HttpHeaderTests.cs
+++ src/Crews.Web.Netjac.Tests/Utility/HttpHeaderTests.cs
@@ -1,4 +1,4 @@
-using System;
+using Crews.Web.Netjac.Utility;
 
 namespace Crews.Web.Netjac.Tests.Utility;
 
@@ -8,8 +8,47 @@ public class HttpHeaderTests
 	[InlineData("Content-Type", "application/json", "Content-Type:application/json")]
 	[InlineData("   Authorization ", "\tBasic abcdefgh  ", "Authorization:Basic abcdefgh")]
 	[InlineData("X-Custom-Header", "ABC123 !#$&'()*+,/:;=?@[]", "X-Custom-Header:ABC123 !#$&'()*+,/:;=?@[]")]
-	public void ToString_CorrectlyFormatsValidStrings(string name, string value, string expected)
+	public void ToStringCorrectlyFormatsValidStrings(string name, string value, string expected)
 	{
+		HttpHeader header = new(name, value);
+		Assert.Equal(expected, header.ToString());
+	}
 
+	[Fact]
+	public void StringImplicitOperatorReturnsValidString()
+	{
+		HttpHeader header = new("X-Custom-Header", "ABC123");
+		string expected = "X-Custom-Header:ABC123";
+		Assert.Equal(expected, header);
+	}
+
+	[Fact]
+	public void StringImplicitOperatorAndToStringAreIdentical()
+	{
+		HttpHeader header = new("X-Custom-Header", "ABC123");
+		Assert.Equal(header, header.ToString());
+	}
+
+	[Fact]
+	public void ClassImplicitOperatorReturnsValidObject()
+	{
+		string expectedName = "X-Custom-Header";
+		string expectedValue = "ABC123";
+
+		string headerString = $"{expectedName}:{expectedValue}";
+		HttpHeader header = headerString;
+
+		Assert.Equal(expectedName, header.Name);
+		Assert.Equal(expectedValue, header.Value);
+	}
+
+	[Theory]
+	[InlineData("Content-Type:application/json", "Content-Type:application/json")]
+	[InlineData("   Authorization  :\tBasic abcdefgh  ", "Authorization:Basic abcdefgh")]
+	[InlineData("X-Custom-Header:ABC123 !#$&'()*+,/:;=?@[]", "X-Custom-Header:ABC123 !#$&'()*+,/:;=?@[]")]
+	public void DirectConstructorFormatsHeaderCorrectly(string input, string expected)
+	{
+		HttpHeader header = new(input);
+		Assert.Equal(expected, header.ToString());
 	}
 }
--- src/Crews.Web.Netjac/Utility/HttpHeader.cs
+++ src/Crews.Web.Netjac/Utility/HttpHeader.cs
@@ -1,37 +1,76 @@
-using System;
-
 namespace Crews.Web.Netjac.Utility;
 
-public struct HttpHeader
+/// <summary>
+/// Represents an HTTP header.
+/// </summary>
+public readonly struct HttpHeader
 {
+	/// <summary>
+	/// The header's name.
+	/// </summary>
 	public string Name { get; }
+
+	/// <summary>
+	/// The header's value.
+	/// </summary>
 	public string Value { get; }
 
+	/// <summary>
+	/// Creates a new HTTP header object from the given string.
+	/// </summary>
+	/// <remarks>
+	/// Due to the unpredictable nature of value delimiters in HTTP headers, a single value will always be assumed. If the
+	/// header string contains multiple values, they must be separated manually. Then, a unique <c>HttpHeader</c> instance
+	/// should be created for each of the values.
+	/// </remarks>
+	/// <param name="header">The string representation of the header.</param>
 	public HttpHeader(string header)
 	{
-		string[] parts = header.Split(':');
-		if (parts.Length != 2)
-		{
-			throw new FormatException("Header must be in the format [name:value].");
-		}
+		string[] parts = header.Split(':', 2);
 
 		Name = parts[0].Trim();
 		Value = parts[1].Trim();
 	}
 
+	/// <summary>
+	/// Creates a new HTTP header object from the given name and value.
+	/// </summary>
+	/// <param name="name">The name of the HTTP header.</param>
+	/// <param name="value">The value or combined values of the HTTP header.</param>
+	/// <exception cref="ArgumentException">
+	/// The header delimiter character was found in the <c>name</c> argument.
+	/// </exception>
+	/// <remarks>
+	/// Due to the unpredictable nature of value delimiters in HTTP headers, a single value will always be assumed. If the
+	/// header string contains multiple values, they must be separated manually. Then, a unique <c>HttpHeader</c> instance
+	/// should be created for each of the values.
+	/// </remarks>
 	public HttpHeader(string name, string value)
 	{
-		foreach (string[] part in new string[][] { [name, nameof(name)], [value, nameof(value)] })
+		if (name.Contains(':'))
 		{
-			if (part[0].Contains(':'))
-			{
-				throw new ArgumentException("Delimiter found in header name or value.", part[1]);
-			}
+			throw new ArgumentException("Delimiter found in header name.", nameof(name));
 		}
 
 		Name = name.Trim();
 		Value = value.Trim();
 	}
 
-    public override string ToString() => $"{Name}:{Value}";
+	/// <summary>
+	/// Gets the string representation of the header in the format <c>name:value</c>.
+	/// </summary>
+	/// <returns>A string representation of the header.</returns>
+	public override readonly string ToString() => $"{Name}:{Value}";
+
+	/// <summary>
+	/// Implicitly creates a new instance using the default constructor.
+	/// </summary>
+	/// <param name="header">The string representation of the header.</param>
+	public static implicit operator HttpHeader(string header) => new(header);
+
+	/// <summary>
+	/// Implicitly converts the given instance to a string. Equivalent to calling <c>ToString()</c>.
+	/// </summary>
+	/// <param name="header">The current instance.</param>
+	public static implicit operator string(HttpHeader header) => header.ToString();
 }
